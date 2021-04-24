b292b3854ddb131cb28b1c17d4de9dc65f47a38c
pengkaiyang <476922814@qq.com>
2021-04-24 21:56:54 +0800

    koa

---
+++ koa/lib/context.js
@@ -0,0 +1,41 @@
+let proto = {};
+
+// 为proto名为property的属性设置setter
+function delegateSet(property, name) {
+    proto.__defineSetter__(name, function (val) {
+        this[property][name] = val;
+    });
+}
+
+// 为proto名为property的属性设置getter
+function delegateGet(property, name) {
+    proto.__defineGetter__(name, function () {
+        return this[property][name];
+    });
+}
+
+// 定义request中要代理的setter和getter
+let requestSet = [];
+let requestGet = ['query'];
+
+// 定义response中要代理的setter和getter
+let responseSet = ['body', 'status'];
+let responseGet = responseSet;
+
+requestSet.forEach(ele => {
+    delegateSet('request', ele);
+});
+
+requestGet.forEach(ele => {
+    delegateGet('request', ele);
+});
+
+responseSet.forEach(ele => {
+    delegateSet('response', ele);
+});
+
+responseGet.forEach(ele => {
+    delegateGet('response', ele);
+});
+
+module.exports = proto;
